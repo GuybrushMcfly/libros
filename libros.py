3521963115051a9e24a862ff826063507711e0e1
GuybrushMcfly <113950867+GuybrushMcfly@users.noreply.github.com>
2025-07-16 19:29:26 -0300

    Update libros.py

--- libros.py
+++ libros.py
@@ -6,7 +6,7 @@ import re
 # --- Conexión a Supabase ---
 SUPABASE_URL = st.secrets["supabase_url"]
 SUPABASE_SERVICE_KEY = st.secrets["supabase_key"]
-supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+supabase = create_client(SUPABASE_URL, SUPABASE_SERVICE_KEY)
 
 # --- Configuración inicial ---
 st.set_page_config(layout="wide", page_title="Gestión Librería", page_icon="📚")
