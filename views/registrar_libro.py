c8dbe529deea3d8e942b555c40d9cc7f9122b3c2
GuybrushMcfly <113950867+GuybrushMcfly@users.noreply.github.com>
2025-07-17 23:57:09 -0300

    Update registrar_libro.py

--- views/registrar_libro.py
+++ views/registrar_libro.py
@@ -117,10 +117,10 @@ def registrar_libro():
             # palabras_clave = st.text_input("Palabras clave (coma)")
 
             col_a, col_b, col_c, col_d = st.columns(4)
-            precio_costo = col_a.number_input("💰 Precio de compra", min_value=0.0, step=0.01)
-            precio_venta = col_b.number_input("🏷️ Precio de venta", min_value=0.0, step=0.01)
-            cantidad = col_c.number_input("📦 Cantidad en stock", min_value=1, step=1)
-            tipo_ingreso = col_d.selectbox("Tipo ingreso", ["- Seleccioná -", "Stock Heredado", "Stock Nuevo"])
+            precio_costo = col_b.number_input("💰 Precio de compra", min_value=0.0, step=0.01)
+            precio_venta = col_c.number_input("🏷️ Precio de venta", min_value=0.0, step=0.01)
+            cantidad = col_d.number_input("📦 Cantidad en stock", min_value=1, step=1)
+            tipo_ingreso = col_a.selectbox("Tipo ingreso", ["- Seleccioná -", "Stock Heredado", "Stock Nuevo"])
 
             # --- CAMPO ACTIVO: Observaciones como último ---
             observaciones = st.text_area("Observaciones")
