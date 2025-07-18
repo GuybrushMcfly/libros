e00c26c5d01e28f4cc6c7d2f8c2cc90f6ecacc7d
GuybrushMcfly <113950867+GuybrushMcfly@users.noreply.github.com>
2025-07-17 22:35:51 -0300

    Update auth.py

--- modules/auth.py
+++ modules/auth.py
@@ -59,20 +59,14 @@ def login():
         "name": "libreria_sesion"
     }
 
+
     authenticator = stauth.Authenticate(
         credentials=credentials,
         cookie_name=credentials["cookie"]["name"],
         cookie_key=credentials["cookie"]["key"],
-        cookie_expiry_days=credentials["cookie"]["expiry_days"],
-        # 👇 Diccionario de traducciones
-        translations={
-            "login": "Iniciar sesión",
-            "username": "Usuario",
-            "password": "Contraseña",
-            "login_button": "Ingresar",
-            "logout": "Cerrar sesión"
-        }
+        cookie_expiry_days=credentials["cookie"]["expiry_days"]
     )
+
     # --- Login centrado y compacto ---
     col1, col2, col3 = st.columns([4, 4, 4])  # Más angosto aún
     with col2:
